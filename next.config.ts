96d2b325d112754a11181617aab297e54138f928
Paul Butler <paul@driftingin.space>
2024-11-26 22:45:56 +0100

    add stats pixel

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+
+  async rewrites() {
+    return [
+      {
+        source: '/ct/js/script.js',
+        destination: 'https://plausible.io/js/script.js',
+      },
+      {
+        source: '/ct/api/event',
+        destination: 'https://plausible.io/api/event',
+      },
+    ]
+  },
   /* config options here */
 }
 
