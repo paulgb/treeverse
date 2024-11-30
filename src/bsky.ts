cb49067e82ad354bd0941a5639fe6491ed421f05
Paul Butler <paulgb@gmail.com>
2024-11-30 09:52:39 +0100

    Add local cache (#103)
    * Add local cache
    
    * add cache ts

--- src/bsky.ts
+++ src/bsky.ts
@@ -1,11 +1,24 @@
+import { getCachedResponse, setCachedResponse } from './cache'
+
 export async function getPosts(user: string, post: string): Promise<AtProtoThreadResponse> {
   const atUri = `at://${user}/app.bsky.feed.post/${post}`
   const url = new URL('https://public.api.bsky.app/xrpc/app.bsky.feed.getPostThread')
   url.searchParams.set('uri', atUri)
   url.searchParams.set('depth', '6')
 
+  // Try to get cached response first
+  const cachedData = await getCachedResponse(url.toString())
+  if (cachedData) {
+    return cachedData
+  }
+
+  // If not in cache, make the fetch request
   const res = await fetch(url)
   const json = await res.json()
+
+  // Cache the response for future use
+  await setCachedResponse(url.toString(), json)
+
   return json
 }
 
